f8de6388784cf1809945c2f9cbc245b7f29d5c15
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-06-09 17:49:22 +0200

    Refactor.

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -4,12 +4,30 @@ import edu.iis.mto.testreactor.dishwasher.engine.Engine;
 import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertThat;
 
 public class DishWasherTest {
+
+    private WaterPump waterPump;
+    private Engine engine;
+    private DirtFilter dirtFilter;
+    private Door door;
+
+    DishWasher dishWasher;
+
+    @Before
+    public void init() {
+        waterPump = Mockito.mock(WaterPump.class);
+        engine = Mockito.mock(Engine.class);
+        dirtFilter = Mockito.mock(DirtFilter.class);
+        door = Mockito.mock(Door.class);
+
+        dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
+    }
 
     @Test
     public void itCompiles() {
@@ -18,24 +36,16 @@ public class DishWasherTest {
 
     @Test
     public void openDoorShouldResultInDoorOpenStatus() {
-        WaterPump waterPump = Mockito.mock(WaterPump.class);
-        Engine engine = Mockito.mock(Engine.class);
-        DirtFilter dirtFilter = Mockito.mock(DirtFilter.class);
-        Door door = Mockito.mock(Door.class);
-
         ProgramConfiguration exampleProperProgramConfiguration = ProgramConfiguration.builder()
                                                                                      .withProgram(WashingProgram.ECO)
                                                                                      .withFillLevel(FillLevel.HALF)
                                                                                      .withTabletsUsed(true)
                                                                                      .build();
-
-        DishWasher dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
-
         Mockito.when(door.closed()).thenReturn(false);
 
         RunResult runResult = dishWasher.start(exampleProperProgramConfiguration);
         Status expectedStatus = Status.DOOR_OPEN;
+        
         Assert.assertEquals(runResult.getStatus(), expectedStatus);
-
     }
 }
